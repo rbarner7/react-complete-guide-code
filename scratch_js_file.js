da5cabf46e6c0e88d43a6bbb8970892faf4c1758
Ryan Barner <rbarner@lyft.com>
2021-05-22 21:14:34 -0700

    Scratch JS file
    Signed-off-by: Ryan Barner <rbarner@lyft.com>

---
+++ scratch_js_file.js
@@ -0,0 +1,14 @@
+// const myName = 'Max';
+// console.log(myName);
+
+// myName = 'Manu';
+// console.log(myName)
+
+function printMyName(name) {
+  console.log(name);
+}
+
+printMyName();
+printMyName('M');
+
+// can be written as
